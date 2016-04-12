1c2411b8cd9ca0bd33b8cc0739ff4feeccbeef35
Samuel Catalano <samuel.catalano@gmail.com>
2016-04-12 19:56:25 -0300

    First commit

---
+++ avenuecodeios/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  avenuecodeios
+//
+//  Created by Samuel D. N. Catalano on 4/12/16.
+//  Copyright © 2016 Avenue Code. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
