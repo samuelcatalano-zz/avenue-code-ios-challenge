b81464efed26c256190383c0c134ad379f0967cd
Samuel Catalano <samuel.catalano@gmail.com>
2016-04-13 03:00:16 -0300

    Creating MainViewController

--- avenuecodeios/AppDelegate.m
+++ avenuecodeios/AppDelegate.m
@@ -7,6 +7,7 @@
 //
 
 #import "AppDelegate.h"
+#import "MainViewController.h"
 
 @interface AppDelegate ()
 
@@ -17,6 +18,15 @@
 
 - (BOOL)application:(UIApplication *)application didFinishLaunchingWithOptions:(NSDictionary *)launchOptions {
     // Override point for customization after application launch.
+    
+    [[UIApplication sharedApplication] setStatusBarHidden:NO];
+    [[UIApplication sharedApplication] setStatusBarStyle:UIStatusBarStyleBlackOpaque];
+    self.window = [[UIWindow alloc] initWithFrame:[[UIScreen mainScreen] bounds]];
+    
+    MainViewController *controller = [[MainViewController alloc] init];
+    self.window.rootViewController = controller;
+    [self.window makeKeyAndVisible];
+    
     return YES;
 }
 
---
+++ avenuecodeios/MainViewController.h
@@ -0,0 +1,13 @@
+//
+//  MainViewController.h
+//  avenuecodeios
+//
+//  Created by Samuel D. N. Catalano on 4/13/16.
+//  Copyright © 2016 Avenue Code. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface MainViewController : UIViewController
+
+@end
